ba49090d0b4f8db5101955524f430ec41888d24d
Tim Hosey <tim@timcade.tv>
2022-11-12 20:29:39 -0800

    added support part

--- contact.php
+++ contact.php
@@ -1,6 +1,13 @@
   <!-- Contact Section -->
   <div class="w3-padding-64 w3-content w3-text-grey" id="contact">
-    <h2 class="w3-text-light-grey">contact tim</h2>
+    <h2 class="w3-text-light-grey">support meh</h2>
+    <hr style="width:200px" class="w3-opacity">
+    <p>you can support meh via the timcade patreon:</p>
+
+    <p>patreon: <a href="https://patreon.com/timcade">timcade</a></p>
+
+    <p>your support goes toward bandwidth costs, hardware considerations
+      and snacks for the admin.</p>
     <hr style="width:200px" class="w3-opacity">
     <p>you can reach out in a variety of ways:</p>
 
