0a485a56832531783710ded81272f52b812eedfa
Tim Hosey <tim@timcade.tv>
2022-11-12 22:37:08 -0800

    added masto logo to footer

--- footer.php
+++ footer.php
@@ -1,11 +1,6 @@
     <!-- Footer -->
     <footer class="w3-content w3-padding-64 w3-text-grey w3-xlarge">
-    <i class="fa fa-facebook-official w3-hover-opacity"></i>
-    <i class="fa fa-instagram w3-hover-opacity"></i>
-    <i class="fa fa-snapchat w3-hover-opacity"></i>
-    <i class="fa fa-pinterest-p w3-hover-opacity"></i>
-    <i class="fa fa-twitter w3-hover-opacity"></i>
-    <i class="fa fa-linkedin w3-hover-opacity"></i>
+    <img src='img/mastodon_logo.svg' />
   <!-- End footer -->
   </footer>
 
