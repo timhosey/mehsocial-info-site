da26d1ae2c3b26e61f2a10c10c35fdcd0a7320ed
Tim Hosey <tim@timcade.tv>
2022-11-12 20:17:24 -0800

    added getLatestBackup function

--- functions.inc.php
+++ functions.inc.php
@@ -42,4 +42,22 @@ function getStorage(){
   return round( (disk_free_space('.') / disk_total_space('.')) * 100, 1);
 }
 
+function getLatestBackup(){
+  $path = "/backup"; 
+
+  $latest_ctime = 0;
+  $latest_filename = '';    
+
+  $d = dir($path);
+  while (false !== ($entry = $d->read())) {
+    $filepath = "{$path}/{$entry}";
+    // could do also other checks than just checking whether the entry is a file
+    if (is_file($filepath) && filectime($filepath) > $latest_ctime) {
+      $latest_ctime = filectime($filepath);
+      $latest_filename = $entry;
+    }
+  }
+  return $latest_ctime;
+}
+
 ?>
