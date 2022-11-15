80503127ce9661029d9b20d71fc708f4dc608ccf
Tim Hosey <tim@timcade.tv>
2022-11-14 18:25:37 -0800

    adjusted about to not have stats anymore

--- about.php
+++ about.php
@@ -27,20 +27,6 @@
       <div class="w3-dark-grey" style="height:28px;width:<?= $diskUsage ?>%"></div>
     </div><br />
 
-  <div class="w3-content w3-justify w3-text-grey w3-padding-64" id="stats">
-    <h2 class="w3-text-light-grey">stats</h2>
-    <hr style="width:200px" class="w3-opacity">
-    <?php
-      $stats = getStats();
-    ?>
-    <p>
-      mastodon version: <strong><?= $stats['version']; ?></strong><br />
-      current users: <strong><?= $stats['stats']['user_count']; ?></strong><br />
-      posts made: <strong><?= $stats['stats']['status_count']; ?></strong><br />
-      connected domains: <strong><?= $stats['stats']['domain_count']; ?></strong>
-    </p>
-  </div>
-
     <p>
       <?php
         $lastBackup = getLatestBackup();
