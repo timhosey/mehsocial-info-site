4a26526e485f77c95fa539509aba0066842e1cbe
Tim Hosey <tim@timcade.tv>
2022-11-14 18:27:11 -0800

    added hr

--- contact.php
+++ contact.php
@@ -1,6 +1,7 @@
   <!-- Contact Section -->
   <div class="w3-padding-64 w3-content w3-text-grey" id="contact">
     <h2 class="w3-text-light-grey">contact</h2>
+    <hr style="width:200px" class="w3-opacity">
     <p>you can reach out in a variety of ways:</p>
 
     <p>mastodon: <a href="https://meh.social/@timcade">timcade</a></p>
