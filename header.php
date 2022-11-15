5068de502120fd423021332977016473a4729bdf
Tim Hosey <tim@timcade.tv>
2022-11-14 18:09:18 -0800

    adjusted icons

--- header.php
+++ header.php
@@ -30,11 +30,11 @@ body, h1,h2,h3,h4,h5,h6 {font-family: "Montserrat", sans-serif}
     <p>home</p>
   </a>
   <a href="#about" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-user w3-xxlarge"></i>
+    <i class="fa fa-block-question w3-xxlarge"></i>
     <p>about</p>
   </a>
   <a href="#stats" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
-    <i class="fa fa-user w3-xxlarge"></i>
+    <i class="fa fa-signal-bars w3-xxlarge"></i>
     <p>stats</p>
   </a>
   <a href="#moderation" class="w3-bar-item w3-button w3-padding-large w3-hover-black">
